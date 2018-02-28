ae051afbd58ce7e71e670d976f1820663151a7fd
Pablo <pincheirapabloj@gmail.com>
2018-02-28 17:34:39 -0300

    Clases LecturaSuministros - lectura suministros item- lecturas modos conceptos

---
+++ Cooperativa/Model/LecturasSuministros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class LecturasSuministros
+    {
+        public virtual long lesCodigo { get; set; }
+        public virtual DateTime lesFechaAlta { get; set; }
+        public virtual DateTime lesFechaAnterior { get; set; }
+        public virtual string lesPeriodo { get; set; }
+        public virtual long sumNumero { get; set; }
+        public virtual int medNumero { get; set; }
+        public virtual int sruNumero { get; set; }
+        public virtual long lemCodigo { get; set; }
+        public virtual string estCodigo { get; set; }
+        public virtual long cbpNumero { get; set; }
+
+
+    }
+}
---
+++ Cooperativa/Model/LecturasSuministrosItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class LecturasSuministrosItems
+    {
+        public virtual long lesCodigo { get; set; }
+        public virtual long lecCodigo { get; set; }
+        public virtual string lsiDescripcion { get; set; }
+        public virtual long lsiLecturaAnterior { get; set; }
+        public virtual long lsiLecturaActual { get; set; }
+        public virtual long lsiCantidadUnidades { get; set; }
+    }
+}
